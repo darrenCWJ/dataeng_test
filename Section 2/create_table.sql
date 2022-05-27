6d9a6d825bfe809709717da1421ef00cae9940fc
Darren Chua <44729635+darrenCWJ@users.noreply.github.com>
2022-05-28 02:23:16 +0800

    Section 2: Create dockerfile
    Created dockerfile to setup for postgresDB and creating the relevant database

---
+++ Section 2/create_table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE car(
+    serial_number VARCHAR(50) PRIMARY KEY,
+    model         VARCHAR(50),
+    manufacturer  VARCHAR(50),
+    weight_kg     DECIMAL(10,2),
+    price         DECIMAL(10,2)
+);
+
+CREATE TABLE sales_person(
+    sales_person_id   VARCHAR(50) PRIMARY KEY,
+    sales_person_name VARCHAR(50)
+);
+
+CREATE TABLE transaction_record(
+    sales_person_id     VARCHAR(50),
+    serial_number       VARCHAR(50),
+    customer_name       VARCHAR(50),
+    customer_phone      NUMERIC(12),
+    characteristic_car  VARCHAR(100),
+    FOREIGN KEY (sales_person_id) REFERENCES sales_person(sales_person_id),
+    FOREIGN KEY (serial_number) REFERENCES car(serial_number)
+);
+
